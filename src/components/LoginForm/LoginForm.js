1b6c6aa50eca874251910e665a6e3b1e9f5bd250
John <44646589+Junnyyy@users.noreply.github.com>
2022-04-16 20:30:21 -0500

    Fixed login form css
    Error formed due to bootstrap, corrected error

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -40,6 +40,7 @@ export default function Login({ setToken }) {
                 onChange={(e) => setUsername(e.target.value)}
               />
             </label>
+            <br></br>
             <label>
               <p>Password</p>
               <input
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
-import { React } from "react";
-import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
+import React from "react";
+import { Container, Navbar, Nav } from "react-bootstrap";
 
 export default function Navigationbar() {
   return (

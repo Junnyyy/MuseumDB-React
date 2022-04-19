65b520f53d9a33d80ed746b57d9e546e6442022f
Johnny <44646589+Junnyyy@users.noreply.github.com>
2022-04-19 13:59:57 -0500

    Syntax change

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,11 @@
-import React from "react";
+import { React } from "react";
 import "./Home.css";
 
 export default function Home() {
   return (
     <div className="info">
       <div className="textContainer">
-        <h2>Welcome 📢</h2>
+        <h2>Welcome! 📢</h2>
         <p>Hello World</p>
       </div>
     </div>

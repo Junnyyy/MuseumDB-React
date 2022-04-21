a52db038a5e007a79fbe26a1a7bc9a942399d2d8
Johnny <44646589+Junnyyy@users.noreply.github.com>
2022-04-21 16:58:16 -0500

    Home color, report text

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,15 +1,23 @@
 import { React } from "react";
 import "./Home.css";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Home(props) {
   return (
-    <div className="info" style = {{ backgroundImage: `url("https://static.mfah.com/images/mfah-at-home.5545251485772177919.jpg")` , backgroundSize: `cover`}}>
+    <div className="info">
       <div className="textContainer">
-        <h2 className = "welcome">Welcome {props.user}! 📢</h2>
-        <ul className = "link"> 
-          <li><Link to="/Collection" className = "link">Collection</Link></li>
-          <li><Link to="/Shop" className = "link">Gift Shop</Link></li>
+        <h2 className="welcome">Welcome {props.user}! 📢</h2>
+        <ul className="link">
+          <li>
+            <Link to="/Collection" className="link">
+              Collection
+            </Link>
+          </li>
+          <li>
+            <Link to="/Shop" className="link">
+              Gift Shop
+            </Link>
+          </li>
         </ul>
       </div>
     </div>
--- src/pages/Report/Report.js
+++ src/pages/Report/Report.js
@@ -2,9 +2,8 @@ import { React, useState } from "react";
 import { Dropdown, DropdownButton } from "react-bootstrap";
 
 import StoreSalesReport from "./StoreSalesReport";
-import TicketSalesReport from "./TicketSalesReport"
+import TicketSalesReport from "./TicketSalesReport";
 import EmployeeAddressBook from "./EmployeeAddressBook";
-
 
 export default function Report() {
   var formType = <></>;
@@ -16,27 +15,33 @@ export default function Report() {
   switch (value) {
     case "EmployeeAddressBook":
       formType = <EmployeeAddressBook />;
-      break
-    case"TicketSalesReport":
+      break;
+    case "TicketSalesReport":
       formType = <TicketSalesReport />;
-      break
-    case"StoreSalesReport":
-      formType = <StoreSalesReport/>;
-      break
+      break;
+    case "StoreSalesReport":
+      formType = <StoreSalesReport />;
+      break;
   }
 
   return (
     <div className="insert-wrapper">
       <div className="selection">
-        <p>Please select a table to insert new data.</p>
+        <p>Please select a report.</p>
         <DropdownButton
           id="dropdown-basic-button"
           title="Select Table"
           onSelect={handleSelect}
         >
-          <Dropdown.Item eventKey="EmployeeAddressBook">Employee Address Book</Dropdown.Item>
-          <Dropdown.Item eventKey="TicketSalesReport">Ticket Sales Report</Dropdown.Item>
-          <Dropdown.Item eventKey="StoreSalesReport">Store Sales Report</Dropdown.Item>
+          <Dropdown.Item eventKey="EmployeeAddressBook">
+            Employee Address Book
+          </Dropdown.Item>
+          <Dropdown.Item eventKey="TicketSalesReport">
+            Ticket Sales Report
+          </Dropdown.Item>
+          <Dropdown.Item eventKey="StoreSalesReport">
+            Store Sales Report
+          </Dropdown.Item>
         </DropdownButton>
       </div>
       <div className="render">{formType}</div>

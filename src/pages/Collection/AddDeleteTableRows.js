499f7a3926900bfdee725975d200ac0ff47dd93f
Jeremy Crain <jcrain@uh.edu>
2022-04-20 19:08:01 -0500

    Added collection table

---
+++ src/pages/Collection/AddDeleteTableRows.js
@@ -0,0 +1,111 @@
+import { useEffect, useState } from "react"
+import TableRows from "./TableRows"
+import "./Collection.css"
+
+
+function AddDeleteTableRows(){
+
+    const getToken = () => {
+        const tokenString = sessionStorage.getItem("token");
+        const userToken = JSON.parse(tokenString);
+        return userToken?.token;
+      };
+
+      const [artData, setData] = useState([])
+
+      const fetchData = () => {
+          fetch("https://cst2-api.azurewebsites.net/artpiece", {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              authorization: `Bearer ${getToken()}`,
+            },
+            mode: "cors",
+          })
+          .then(response => {
+              return response.json()
+          })
+          .then(data => {
+              setData(data)
+          })
+      }
+
+    useEffect(() => {
+          fetchData()
+      }, [])
+    
+    console.log(artData)
+
+    const [rowsData, setRowsData] = useState([]);
+
+    const addTableRows = ()=>{
+
+        const rowsInput={
+            Art_Piece_Title:'',
+            Date_Created:'',
+            Medium:'',
+            Creator_F_Name:'',
+            Creator_L_Name:'',
+            Being_Refurbished:'',
+            Culture:'',
+            Piece_Height:'',
+            Piece_Length:'',
+            Piece_Width:'',
+            Gallery_Loc:'',
+            Exhibit_ID:''
+        } 
+        setRowsData([...rowsData, rowsInput])
+    }
+
+   const deleteTableRows = (index)=>
+   {
+        const rows = [...rowsData];
+        rows.splice(index, 1);
+        setRowsData(rows);
+   }
+ 
+   const handleChange = (index, evnt)=>
+   {
+    const { name, value } = evnt.target;
+    const rowsInput = [...rowsData];
+    rowsInput[index][name] = value;
+    setRowsData(rowsInput);
+    }
+
+    return(
+        <div className="container" >
+            <div className="row">
+                <div className="col-sm-8">
+
+                <table className="table">
+                    <thead>
+                      <tr>
+                          <th>Title</th>
+                          <th>Date Created</th>
+                          <th>Medium</th>
+                          <th>First Name</th>
+                          <th>Last Name</th>
+                          <th>Refurbished</th>
+                          <th>Culture</th>
+                          <th>Height</th>
+                          <th>Length</th>
+                          <th>Width</th>
+                          <th>Gallery</th>
+                          <th>Exhibit ID</th>
+                          <th><button className="btn btn-outline-success" onClick={addTableRows} >+</button></th>
+                      </tr>
+                    </thead>
+                   <tbody>
+                   <TableRows rowsData={artData} deleteTableRows={deleteTableRows} handleChange={handleChange} />
+                   <TableRows rowsData={rowsData} deleteTableRows={deleteTableRows} handleChange={handleChange} />
+                   </tbody> 
+                </table>
+
+                </div>
+
+            </div>
+        </div>
+    )
+
+}
+export default AddDeleteTableRows
---
+++ src/pages/Collection/TableRows.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react"
+import "./Collection.css"
+
+function TableRows({rowsData, deleteTableRows, handleChange}) {
+
+
+    return(
+        
+        rowsData.map((data, index)=>{
+            const {Art_Piece_Title, Date_Created, Medium, Creator_F_Name, Creator_L_Name, Being_Refurbished, Culture,Piece_Height, Piece_Length, Piece_Width,Gallery_Loc, Exhibit_ID}= data;
+            return(
+
+                <tr key={index}>
+                <td><input type="text" value={Art_Piece_Title} onChange={(evnt)=>(handleChange(index, evnt))} name="Art_Piece_Title" className="form-control"/> </td>
+                <td><input type="text" value={Date_Created}  onChange={(evnt)=>(handleChange(index, evnt))} name="Date_Created" className="form-control"/> </td>
+                <td><input type="text" value={Medium}  onChange={(evnt)=>(handleChange(index, evnt))} name="Medium" className="form-control" /> </td>
+                <td><input type="text" value={Creator_F_Name} onChange={(evnt)=>(handleChange(index, evnt))} name="Creator_F_Name" className="form-control"/> </td>
+                <td><input type="text" value={Creator_L_Name}  onChange={(evnt)=>(handleChange(index, evnt))} name="Creator_L_Name" className="form-control"/> </td>
+                <td><input type="text" value={Being_Refurbished}  onChange={(evnt)=>(handleChange(index, evnt))} name="Being_Refurbished" className="form-control" /> </td>
+                <td><input type="text" value={Culture} onChange={(evnt)=>(handleChange(index, evnt))} name="Culture" className="form-control"/> </td>
+                <td><input type="text" value={Piece_Height}  onChange={(evnt)=>(handleChange(index, evnt))} name="Piece_Height" className="form-control"/> </td>
+                <td><input type="text" value={Piece_Length}  onChange={(evnt)=>(handleChange(index, evnt))} name="Piece_Length" className="form-control" /> </td>
+                <td><input type="text" value={Piece_Width} onChange={(evnt)=>(handleChange(index, evnt))} name="Piece_Width" className="form-control"/> </td>
+                <td><input type="text" value={Gallery_Loc}  onChange={(evnt)=>(handleChange(index, evnt))} name="Gallery_Loc" className="form-control"/> </td>
+                <td><input type="text" value={Exhibit_ID}  onChange={(evnt)=>(handleChange(index, evnt))} name="Exhibit_ID" className="form-control" /> </td>
+                <td><button className="btn btn-outline-danger" onClick={()=>(deleteTableRows(index))}>x</button></td>
+            </tr>
+
+            )
+        })
+   
+    )
+    
+}
+
+export default TableRows;

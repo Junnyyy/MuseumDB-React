98c1db41eb1fbf1334126c15cb31e6ac50042271
John <44646589+Junnyyy@users.noreply.github.com>
2022-04-24 17:27:31 -0500

    Artpiece search

---
+++ src/components/Search/artdatatable.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { Table } from "react-bootstrap";
+
+export default function ArtDatatable({ data }) {
+  const columns = data[0] && Object.keys(data[0]);
+  return (
+    // <table cellPadding={0} cellSpacing={0}>
+    // <Table id="table" className="table">
+    <>
+      <Table responsive className="datatable">
+        <thread>
+          {/* <tr>{data[0] && columns.map((heading) => <th>{heading}</th>)}</tr> */}
+          <tr>
+            <th>ID #</th>
+            <th>Title</th>
+            <th>Data created</th>
+            <th>Medium</th>
+            <th>Creator firstname</th>
+            <th>Creator lastname</th>
+            <th>Being refurbished</th>
+            <th>On display</th>
+            <th>Year acquired</th>
+            <th>Culture</th>
+            <th>Height</th>
+            <th>Length</th>
+            <th>Width</th>
+            <th>Location</th>
+            <th>Exhibit ID</th>
+          </tr>
+        </thread>
+        <tbody>
+          {/* {data.map((data, index) => {
+            return (
+              <tr key={index}>
+                <td type="text">{data.Art_Piece_ID}</td>
+                <td type="text"> {data.Art_Piece_Title}</td>
+                <td type="text">{data.Data_Created} </td>
+                <td type="text">{data.Medium}</td>
+                <td type="text">{data.Department_Name} </td>
+                <td type="text">{data.Employee_Email}</td>
+                <td type="text"> {data.Employee_F_Name}</td>
+                <td type="text">{data.Employee_M_Name} </td>
+                <td type="text">{data.Employee_L_Name}</td>
+                <td type="text">{data.Department_Name} </td>
+                <td type="text">{data.Employee_Email}</td>
+                <td type="text"> {data.Employee_F_Name}</td>
+                <td type="text">{data.Employee_M_Name} </td>
+                <td type="text">{data.Employee_L_Name}</td>
+                <td type="text">{data.Department_Name} </td>
+                <td type="text">{data.Employee_Email}</td>
+              </tr>
+            );
+          })} */}
+          {data.map((row) => (
+            <tr>
+              {columns.map((column) => (
+                <td type="text">{row[column]}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </>
+  );
+}
--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,10 +1,124 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Dropdown, DropdownButton } from "react-bootstrap";
+import "./Search.css";
+
+import Artpiece from "../../components/Search/Options/artpiece";
 
 export default function Search() {
+  var searchType = <></>;
+  const [option, setOptions] = useState("");
+  const handleSelect = (e) => {
+    setOptions(e);
+  };
+
+  switch (option) {
+    case "artpiece":
+      searchType = <Artpiece />;
+      break;
+    // case "customer":
+    //   searchType = (
+    //     <Customer
+    //       setType={setType}
+    //       setValid={setValid}
+    //       setMessage={setMessage}
+    //     />
+    //   );
+    //   break;
+    // case "department":
+    //   searchType = (
+    //     <Department
+    //       setType={setType}
+    //       setValid={setValid}
+    //       setMessage={setMessage}
+    //     />
+    //   );
+    //   break;
+    // case "employee":
+    //   searchType = (
+    //     <Employee
+    //       setType={setType}
+    //       setValid={setValid}
+    //       setMessage={setMessage}
+    //     />
+    //   );
+    //   break;
+    // case "exhibit":
+    //   searchType = (
+    //     <Exhibit
+    //       setType={setType}
+    //       setValid={setValid}
+    //       setMessage={setMessage}
+    //     />
+    //   );
+    //   break;
+    // case "gallery":
+    //   searchType = (
+    //     <Gallery
+    //       setType={setType}
+    //       setValid={setValid}
+    //       setMessage={setMessage}
+    //     />
+    //   );
+    //   break;
+    // case "store-item":
+    //   searchType = (
+    //     <StoreItem
+    //       setType={setType}
+    //       setValid={setValid}
+    //       setMessage={setMessage}
+    //     />
+    //   );
+    //   break;
+    // case "store-transaction":
+    //   searchType = (
+    //     <Store_Transaction
+    //       setType={setType}
+    //       setValid={setValid}
+    //       setMessage={setMessage}
+    //     />
+    //   );
+    //   break;
+    // case "ticket-transaction":
+    //   searchType = (
+    //     <Ticket_Transaction
+    //       setType={setType}
+    //       setValid={setValid}
+    //       setMessage={setMessage}
+    //     />
+    //   );
+    //   break;
+  }
+
   return (
     <div>
-      <br></br>
-      <h2>Search</h2>
+      <div className="selection">
+        <h2>Please select a table to search.</h2>
+        <DropdownButton
+          id="dropdown-basic-button"
+          title="Select Table"
+          onSelect={handleSelect}
+        >
+          <Dropdown.Item eventKey="artpiece">Art Piece</Dropdown.Item>
+          <Dropdown.Item eventKey="customer">Customer</Dropdown.Item>
+          <Dropdown.Item eventKey="department">Department</Dropdown.Item>
+          <Dropdown.Item eventKey="employee">Employee</Dropdown.Item>
+          <Dropdown.Item eventKey="exhibit">Exhibit</Dropdown.Item>
+          <Dropdown.Item eventKey="gallery">Gallery</Dropdown.Item>
+          <Dropdown.Item eventKey="store-item">Store Item</Dropdown.Item>
+          <Dropdown.Item eventKey="store-transaction">
+            {" "}
+            Store Transaction
+          </Dropdown.Item>
+          <Dropdown.Item eventKey="ticket-transaction">
+            {" "}
+            Ticket Transaction
+          </Dropdown.Item>
+        </DropdownButton>
+      </div>
+      <div className="render">{searchType}</div>
+      {/* <br></br>
+      <h2>Search</h2> */}
+      {/* <Artpiece /> */}
     </div>
   );
 }
